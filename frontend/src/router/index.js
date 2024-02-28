09860b2bb6e1cfbaf555a7431f02e6688f63ee7a
omar tarik yehya <khalaf.omar20@gmail.com>
2024-02-28 08:07:24 +0800

    feat: created protected routes
    User can't access main website features without having token

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,68 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from '../views/Dashboard';
+import List from '../views/List'
 import Login from '../views/Login';
 import Register from '../views/Register';
+import DeafultLayout from '../components/DeafultLayout';
+import AuthLayout from '../components/AuthLayout';
+import store from "@/store";
 
 const routes = [
+
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: AuthLayout,
+        meta: {isGuest: true},
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            },
+        ]
     },
     {
         path: '/',
-        name: 'Dashboard',
-        component: Dashboard
+        redirect: '/dashboard',
+        component: DeafultLayout,
+        meta: {requiresAuth: true},
+        children: [
+            {
+                path: '/dashboard',
+                name: 'Dashboard',
+                component: Dashboard
+            },
+            {
+                path: '/list',
+                name: 'List',
+                component: List
+            }
+        ]
     }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,  
+})
+
+router.beforeEach((to , from , next) => {
+    if(to.meta.requiresAuth && !store.state.user.token){
+        next({name: 'Login'})
+    }
+    else if (store.state.user.token && (to.meta.isGuest)) {
+        next({name: 'Dashboard'});
+    }
+    else {
+        next();
+    }
 })
 
 export default router;
